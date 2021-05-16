d75e350b788a1114f4b50377f89bacf98dd99138
Andrew <e.trder@gmail.com>
2021-05-16 19:37:03 +0300

    Added own model

---
+++ example-prjs/model_1/hls4ml_prj/firmware/weights/b11.h
@@ -0,0 +1,15 @@
+//Numpy array shape [5]
+//Min -0.133229762316
+//Max 0.070060126483
+//Number of zeros 0
+
+#ifndef B11_H_
+#define B11_H_
+
+#ifndef __SYNTHESIS__
+model_default_t b11[5];
+#else
+model_default_t b11[5] = {-0.1332297623, 0.0700601265, 0.0547326617, 0.0113842264, -0.0189647693};
+#endif
+
+#endif
